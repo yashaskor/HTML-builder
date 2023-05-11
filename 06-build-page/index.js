6666a816fdb438b48ca167f7aa9ff6f4a6806a98
yashaskor <ya1985.85@gmail.com>
2023-05-11 18:39:31 +0300

    feat: change 06-build-page code to work when del or add files or change code

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,112 +1,69 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
-const COMPONENTS_DIR = path.join(__dirname,'components');
-const STYLES_DIR = path.join(__dirname,'styles');
-const ASSETS_DIR = path.join(__dirname,'assets');
-const TEMPLATE_FILE = path.join(__dirname,'template.html');
-const PROJECT_DIST_DIR = path.join(__dirname,'project-dist');
-const INDEX_FILE = path.join(__dirname, 'project-dist', 'index.html');
-const STYLE_FILE = path.join(__dirname, 'project-dist', 'style.css');
+const componentsDir = path.join(__dirname, 'components');
+const stylesDir = path.join(__dirname, 'styles');
+const assetsDir = path.join(__dirname, 'assets');
+const templateFile = path.join(__dirname, 'template.html');
+const distDir = path.join(__dirname, 'project-dist');
+const indexFile = path.join(distDir, 'index.html');
+const distStyleFile = path.join(distDir, 'style.css');
 
-const componentsMap = new Map();
-let stylesContent = '';
+async function copyRecursive(src, dest) {
+  const exists = await fs.stat(src);
+  if (exists.isDirectory()) {
+    await fs.mkdir(dest, { recursive: true });
+    const childItems = await fs.readdir(src);
+    for (const childItem of childItems) {
+      const childSrc = path.join(src, childItem);
+      const childDest = path.join(dest, childItem);
+      await copyRecursive(childSrc, childDest);
+    }
+  } else {
+    await fs.copyFile(src, dest);
+  }
+}
 
 async function main() {
   try {
-    await createProjectDistDir();
-    await processComponents();
-    await processStyles();
-    await copyAssets();
-    console.log('Success: build completed');
-  } catch (err) {
-    console.error(`Error: ${err.message}`);
-  }
-}
+    // Create dist directory if it doesn't exist
+    if (!await fs.stat(distDir).catch(() => false)) {
+      await fs.mkdir(distDir);
+    }
 
-async function createProjectDistDir() {
-  await fs.promises.mkdir(PROJECT_DIST_DIR);
-}
-
-async function processComponents() {
-  const templateContent = await fs.promises.readFile(TEMPLATE_FILE, 'utf8');
-  const promises = templateContent.split(/\{\{(.+?)\}\}/)
-    .map(async (token, i) => {
-      if (i % 2 === 0) {
-        // even indexes contain text between tokens
-        return token;
-      } else {
-        // odd indexes contain token names
-        const componentName = token.trim();
-        if (componentsMap.has(componentName)) {
-          return componentsMap.get(componentName);
-        } else {
-          const componentPath = path.join(COMPONENTS_DIR, `${componentName}.html`);
-          const componentContent = await fs.promises.readFile(componentPath, 'utf8');
-          componentsMap.set(componentName, componentContent);
-          return componentContent;
-        }
-      }
-    });
-  const indexContent = (await Promise.all(promises)).join('');
-  await fs.promises.writeFile(INDEX_FILE, indexContent);
-}
-
-async function processStyles() {
-  const styleFiles = await fs.promises.readdir(STYLES_DIR);
-  const promises = styleFiles
-    .filter(filename => filename.endsWith('.css'))
-    .map(async filename => {
-      const filePath = path.join(STYLES_DIR, filename);
-      const fileContent = await fs.promises.readFile(filePath, 'utf8');
-      stylesContent += fileContent;
-    });
-  await Promise.all(promises);
-  await fs.promises.writeFile(STYLE_FILE, stylesContent);
-}
-
-async function copyAssets() {
-  const srcDir = ASSETS_DIR;
-  const destDir = path.join(__dirname, 'project-dist', 'assets');;
-  try {
-    await copyDir(srcDir, destDir);
-    console.log('Success: assets copied');
-  } catch (err) {
-    console.error(`Error: ${err.message}`);
-  }
-}
-
-async function copyDir(src, dest) {
-  await fs.promises.mkdir(dest, { recursive: true });
-  const entries = await fs.promises.readdir(src, { withFileTypes: true });
-  for (const entry of entries) {
-    const srcPath = path.join(src, entry.name);
-    const destPath = path.join(dest, entry.name);
-    if (entry.isDirectory()) {
-      await copyDir(srcPath, destPath);
-    } else {
-      const srcFileStat = await fs.promises.stat(srcPath);
-      const destFileStat = await getFileStat(destPath);
-      if (!destFileStat || srcFileStat.mtimeMs > destFileStat.mtimeMs) {
-        await fs.promises.copyFile(srcPath, destPath);
+    // Read template file and replace component tags
+    let template = await fs.readFile(templateFile, 'utf-8');
+    const componentFiles = await fs.readdir(componentsDir);
+    for (const componentFile of componentFiles) {
+      const componentName = path.basename(componentFile, path.extname(componentFile));
+      const componentTag = `{{${componentName}}}`;
+      if (template.includes(componentTag)) {
+        const componentFilePath = path.join(componentsDir, componentFile);
+        const componentContent = await fs.readFile(componentFilePath, 'utf-8');
+        template = template.replace(new RegExp(componentTag, 'g'), componentContent);
       }
     }
+    await fs.writeFile(indexFile, template);
+
+    // Combine all css files into one
+    const styleFiles = await fs.readdir(stylesDir);
+    let styleContent = '';
+    for (const styleFile of styleFiles) {
+      const styleFilePath = path.join(stylesDir, styleFile);
+      if (path.extname(styleFilePath) === '.css') {
+        const styleFileContent = await fs.readFile(styleFilePath, 'utf-8');
+        styleContent += styleFileContent;
+      }
+    }
+    await fs.writeFile(distStyleFile, styleContent);
+
+    // Copy assets directory recursively
+    const assetsSrcPath = path.resolve(assetsDir);
+    const assetsDestPath = path.resolve(distDir, 'assets');
+    await copyRecursive(assetsSrcPath, assetsDestPath);
+  } catch (error) {
+    console.error(error);
   }
 }
-        
-        async function getFileStat(filePath) {
-        try {
-        return await fs.promises.stat(filePath);
-        } catch (err) {
-        if (err.code === 'ENOENT') {
-        return null;
-        } else {
-        throw err;
-        }
-        }
-        }
-    
-  
-
 
 main();
